f2d78fe9532c21c514c73486eda9ca4bf1e30cf4
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2022-02-28 21:45:01 -0300

    practica-directivas-con-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practicas-con-angular02';
+  helloWord = () => {
+    return 'Hola Mundo (desde una funcion)';
+  };
+  pi: number = 3.14159265359;
+  numeros: number[] = [1, 2, 3, 4, 5];
+  //Practica de la pagina de ArgPrograma
+  armaduras = [
+    'Mark I',
+    'Mark II',
+    'Mark III',
+    'Mark IV',
+    'Mark V',
+    'Mark VI',
+    'Mark XLII',
+  ];
+  ironman = 'Tony Stark';
+  revelar_identidad = true; // cambiar por true
+  usar = this.armaduras[2];
+  superhero = 'Ironman';
 }
